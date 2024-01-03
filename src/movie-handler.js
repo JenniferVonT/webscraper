218a1e31662a402cebdc5277084bc2cf507d1692
Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
2024-01-03 02:00:48 +0100

    Fix the availibleMovies method in the MovieHandler class so that the movie values are correct and in turn the request URL becomes correct to obtain the JSON objects for the movie availability

--- src/movie-handler.js
+++ src/movie-handler.js
@@ -34,37 +34,27 @@ export class MovieHandler {
     // Get the select element for the day form.
     const selectDay = dom('select[id="day"]')
     // Set the value on the the select element to the corresponding day.
-    const dayValue = selectDay.find(`option:contains(${day})`)
-    selectDay.val(dayValue.val())
+    const dayValue = selectDay.find(`option:contains(${day})`).val()
+    selectDay.val(dayValue)
 
     // Get all the values for the option elements.
     const selectMovie = dom('select[id="movie"]')
     const movieOptions = selectMovie.find('option[value]')
-    const movieValues = []
-
-    movieOptions.each((index, element) => {
-      movieValues.push(dom(element).val())
-    })
+    const movieValues = movieOptions.map((index, element) => dom(element).val()).get()
 
     // Start iterating through the movies.
     for (let i = 0; i < movieValues.length; i++) {
-      const value = movieValues[i]
-
       // Make a request for each movie.
-      const movieResponse = await axios.get(url, {
+      const movieResponse = await axios.get(`${url}/check`, {
         params: {
           day: dayValue,
-          movie: value
+          movie: movieValues[i]
         }
       })
 
-      // Get the updated HTML and DOM.
-      const updatedHTML = movieResponse.data
-      const updatedDOM = cheerio.load(updatedHTML)
-
-      // const availableTimes = updatedDOM('div[id="message"]').text()
-
-      const submitButton = dom('button[id="check"]')
+      // Get the response object from the request.
+      const responseData = movieResponse.data
+      console.log(responseData)
     }
   }
 }
